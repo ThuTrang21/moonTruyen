06d0227d56d699c755e79f9513648521c353ada5
ThuTrang21 <thut15794@gmail.com>
2024-11-13 00:29:24 +0700

    Update navbar

--- moontruyen_fe/src/Component/Navbar/Navbar.jsx
+++ moontruyen_fe/src/Component/Navbar/Navbar.jsx
@@ -63,12 +63,16 @@ export const Navbar = () => {
     <div className="bg-primary flex items-center h-16 inset-0 dark:bg-black dark:text-white duration-200">
       <div className="px-3.5 flex items-center mx-auto w-full max-w-7xl">
         <div className="flex items-center gap-x-3 z-[999] relative">
-          <img src={logo} alt="" className="w-44 sm:cursor-pointer dark:hidden" />
           <img
-      src={logoLight}
-      alt=""
-      className="w-44 sm:cursor-pointer hidden dark:block"
-    />
+            src={logo}
+            alt=""
+            className="w-44 sm:cursor-pointer dark:hidden"
+          />
+          <img
+            src={logoLight}
+            alt=""
+            className="w-44 sm:cursor-pointer hidden dark:block"
+          />
         </div>
 
         {/* menu */}
@@ -86,15 +90,17 @@ export const Navbar = () => {
 
         {/* navbar right */}
         <div className="flex items-center gap-x-5 z-[999] relative ml-auto ">
-          <div className="hidden sm:block group">
+          <div className="hidden sm:block group mr-10 relative">
             <input
               type="text"
               placeholder="Nhập Tên Truyện..."
-              className="dark:text-black w-[200px] 
-              group-hover:w-[250px] transition-all duration-300 rounded-full border border-gray-300 px-3 py-1 focus:outline-none focus:border-1 focus:border-blue-400"
+              className="dark:text-black w-[200px] transition-all duration-300 rounded-full border border-gray-300 px-3 py-1 focus:outline-none focus:border-1 focus:border-blue-400 pl-10" // Thêm padding-left để tạo không gian cho icon
             />
-            <SearchIcon className="text-gray-500 group-hover:text-primary -translate-x-7" />
+            <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+              <SearchIcon className="text-gray-500 group-hover:text-primary" />
+            </div>
           </div>
+
           <div>
             <DarkMode />
           </div>
@@ -102,7 +108,10 @@ export const Navbar = () => {
             <Avatar>T</Avatar>
           ) : (
             <IconButton>
-              <PersonIcon className="dark:text-white" sx={{ fontSize: "33px" }} />
+              <PersonIcon
+                className="dark:text-white"
+                sx={{ fontSize: "33px" }}
+              />
             </IconButton>
           )}
           <div className="lg:hidden">
