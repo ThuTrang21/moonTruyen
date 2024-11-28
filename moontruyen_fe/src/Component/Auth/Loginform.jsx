3ce0fbb3fb20f846e03057bac1c7112e5a38f8b7
thanhlam30 <dothanhlampt30@gmail.com>
2024-11-28 14:14:44 +0700

    config api service, auth, token, local storage

--- moontruyen_fe/src/Component/Auth/Loginform.jsx
+++ moontruyen_fe/src/Component/Auth/Loginform.jsx
@@ -1,16 +1,44 @@
 import { Button, TextField, Typography } from "@mui/material";
 import { Field, Form, Formik } from "formik";
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+import ApiService from '../../services/api.service';
 const initialValues = {
-  email: "",
+  username: "",
   password: "",
 };
 
 export const Loginform = () => {
+  const cookies = new Cookies();
   const navigate = useNavigate();
-  const handleSubmit = (values) => {
-    console.log("tai khoan:", values);
+  const handleSubmit = async (values) => {
+    console.log(values);
+    try {
+      const response = await ApiService.post("auth/token",
+        {
+          username: values.username,
+          password: values.password
+        })
+        if (response.status === 200) {
+          cookies.set("token", response.data.result.token,{
+            path: "/",
+            maxAge: 7 * 24 * 60 * 60,
+          })
+          localStorage.setItem("token", JSON.stringify(response.data.result.token));
+          const myinfo = await ApiService.get("users/my-info")
+          if(myinfo.status === 200){
+            console.log(myinfo.data.result);
+            localStorage.setItem("userInfo", JSON.stringify(myinfo.data.result));
+          }
+          navigate("/");
+        }
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        console.log(err.response.data.message)
+      }
+    }
+    
   };
   return (
     <div>
@@ -21,8 +49,8 @@ export const Loginform = () => {
         <Form>
           <Field
             as={TextField}
-            name="email"
-            label="Email"
+            name="username"
+            label="Username"
             fullWidth
             variant="outlined"
           ></Field>
@@ -48,8 +76,8 @@ export const Loginform = () => {
       <Typography variant="body2" align="center" sx={{ mt: 3 }}>
         <p className="text-gray-500">Bạn chưa có tài khoản?  </p>
         <span onClick={() => navigate("/account/register")}
-            className="text-red-400 cursor-pointer"
-            >
+          className="text-red-400 cursor-pointer"
+        >
           Đăng ký
         </span>
       </Typography>
---
+++ moontruyen_fe/src/services/api.service.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+let BASE_URL = process.env.REACT_APP_API_URL;
+const instance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    
+  },
+  withCredentials: true
+});
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts.pop().split(';').shift();
+  }
+}
+
+instance.interceptors.request.use((config) => {
+  let token = getCookie("token");
+  if (token) {
+    config.headers["Authorization"] = "Bearer " + token;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+})
+
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const status = error.response ? error.response.status : null;
+    
+    if (status === 401) {
+      console.error("Unauthorized!"); 
+    } else if (status === 500) {
+      console.error("Server error!");
+    }
+    return Promise.reject(error);
+  }
+);
+
+
+export default instance
